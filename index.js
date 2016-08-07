d9839b1493597d616d1cc61e28707b141c9fc978
Luca Traversari <trave5@yahoo.it>
2016-08-07 14:34:18 +0000

    Added the possibility to get  the IP address, language and operating system for my browser

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 var express = require('express');
+var app = express();
+var getIP = require('ipware')().get_ip;
+
+app.get("/",function(req,resp){
+    /* <h1>I can get the IP address, language and operating system for my browser</h1> */
+
+
+    var ipInfo = getIP(req);
+   var user_agent = 'User-Agent: ' + req.headers['user-agent'];
+   var language = req.headers["accept-language"];
+   var response_object = {"ipaddress":ipInfo.clientIp,"language":language,"software":user_agent};
+   resp.end(JSON.stringify(response_object));
+
+
+
+})
+app.listen(8080);
